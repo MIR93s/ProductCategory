fac0ebbe19b0828a4dbf6db1f75bc3fc9273f5e5
MIR93s <111383039+MIR93s@users.noreply.github.com>
2022-09-22 00:47:57 +0300

    Add files via upload

---
+++ Authorization/CategoryAdministratorsAuthorizationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using ProductCategories.Models;
+using ProductCategories.Authorization;
+
+namespace ProductCategories.Authorization
+{
+    public class CategoryAdministratorsAuthorizationHandler
+                  : AuthorizationHandler<OperationAuthorizationRequirement, Category>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                       OperationAuthorizationRequirement requirement,
+                                                       Category resource)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+            // Administrators can do anything.
+            if (context.User.IsInRole(Constants.AdministratorsRole))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Authorization/ProductAdministratorsAuthorizationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using ProductCategories.Models;
+using ProductCategories.Authorization;
+
+namespace ProductCategories.Authorization
+{
+    public class ProductAdministratorsAuthorizationHandler
+                  : AuthorizationHandler<OperationAuthorizationRequirement, ProductCategories.Models.Product>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                       OperationAuthorizationRequirement requirement,
+                                                       ProductCategories.Models.Product resource)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+            // Administrators can do anything.
+            if (context.User.IsInRole(Constants.AdministratorsRole))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
